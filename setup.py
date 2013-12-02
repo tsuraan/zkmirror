ba7816dceaa9a747809e3a44511084804a70b768
jay groven <jgroven@lutsendata.com>
2013-12-02 12:30:02 -0600

    make zkmirror easy_install compatible

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='zkmirror',
+      version='0.0.1',
+      description='State mirroring client for ZooKeeper',
+      author='Jay Groven',
+      author_email='tsuraan@gmail.com',
+      url='github.com/tsuraan/zkmirror',
+      packages=['zkmirror'],
+      install_requires = [],
+     )
+
